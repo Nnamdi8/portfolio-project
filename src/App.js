3f185d28d5208dd0b0d4c03c0f6f746e95237a0d
Nnamdi Okoroafor <nnamdiokoroafor567@gmail.com>
2021-09-07 09:26:42 +0100

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+const App =() =>{
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
