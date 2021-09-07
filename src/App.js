3a4923f8e3daefbcf6a7e17b5231018bd02e1367
Nnamdi Okoroafor <nnamdiokoroafor567@gmail.com>
2021-09-07 16:01:43 +0100

    Navba Completedr

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
+import Navbar from './components/Navbar';
 const App =() =>{
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Navbar/>
     </div>
   );
 }
